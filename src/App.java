4c114afed24343e3565434cd87ba7489b413649c
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-04 01:14:10 +0000

    Added the Participant class

---
+++ src/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Mohammed Aouf ZOUAG on 04/03/2016.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Participant.java
@@ -0,0 +1,59 @@
+/**
+ * Created by Mohammed Aouf ZOUAG on 04/03/2016.
+ */
+public class Participant {
+    private String firstname;
+    private String lastname;
+    private String phoneNumber;
+    private String email;
+    private String address;
+
+    public Participant(String firstname, String lastname, String phoneNumber,
+                       String email, String address) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
