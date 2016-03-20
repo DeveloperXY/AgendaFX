3bb885f7f5c2100337948a6293cc3016f5b4c9b4
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-20 16:24:09 +0000

    Reset the status label's text to its original value after adding a new participant.

--- src/gui/controllers/DirectoryController.java
+++ src/gui/controllers/DirectoryController.java
@@ -1,13 +1,17 @@
 package gui.controllers;
 
 import gui.windows.ParticipantDialog;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.Stage;
-import models.Participant;
 import models.observable.ObsParticipant;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 20/03/2016.
@@ -47,6 +51,9 @@ public class DirectoryController extends BaseController {
     private TableColumn<ObsParticipant, String> addressColumn;
 
     @FXML
+    private Label statusLabel;
+
+    @FXML
     private void initialize() {
         firstnameColumn.setCellValueFactory(cell -> cell.getValue().firstnameProperty());
         lastnameColumn.setCellValueFactory(cell -> cell.getValue().lastnameProperty());
@@ -61,9 +68,23 @@ public class DirectoryController extends BaseController {
     @FXML
     public void onAddParticipant() {
         ParticipantDialog window = new ParticipantDialog(mStage);
-        window.setAddListener(participant ->
-                participantsTable.getItems().add(participant));
+        window.setAddListener(participant -> {
+            participantsTable.getItems().add(participant);
+            statusLabel.setText("Entry successfully created.");
+            resetStatusLabel();
+        });
         window.showAndWait();
+    }
+
+    /**
+     * Resets the text of the status label to its original value.
+     */
+    private void resetStatusLabel() {
+        ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+        Runnable task = () -> Platform.runLater(() -> statusLabel.setText("Ready."));
+        executor.schedule(task, 5, TimeUnit.SECONDS);
+
+        executor.shutdown();
     }
 
     @FXML
