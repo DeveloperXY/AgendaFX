98c827525395850ac4c2624556f532baa389b6c6
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-21 12:07:33 +0000

    Initialize the participants' combo box with first names.

--- src/gui/controllers/dialogs/RDVDialogController.java
+++ src/gui/controllers/dialogs/RDVDialogController.java
@@ -1,12 +1,45 @@
 package gui.controllers.dialogs;
 
 import gui.controllers.BaseController;
+import gui.models.ObsParticipant;
+import gui.windows.RDVWindow;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+import java.util.stream.Collectors;
 
 /**
  * Created by Mohammed Aouf ZOUAG on 21/03/2016.
  */
 public class RDVDialogController extends BaseController {
+
+    @FXML
+    private ComboBox<String> participantsCombobox;
+
+    private ObservableList<String> comboboxData;
+
+    @FXML
+    public void initialize() {
+        initializeCombobox();
+    }
+
+    /**
+     * Initializes the combobox of participants.
+     */
+    private void initializeCombobox() {
+
+        // Initialize the combo box with participant names
+        comboboxData = FXCollections.observableArrayList(
+                RDVWindow.getParticipants()
+                        .stream()
+                        .map(ObsParticipant::getFirstname)
+                        .collect(Collectors.toList()));
+
+        participantsCombobox.setItems(comboboxData);
+    }
+
     @FXML
     private void onCancel() {
         mStage.close();
