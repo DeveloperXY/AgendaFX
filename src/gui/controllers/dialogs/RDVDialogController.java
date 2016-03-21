92851bb77a82db28496898c7b64da8e2bb13e734
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-03-21 12:47:58 +0000

    Enlargen the width of the RDVDialog & modify its layout.

--- src/gui/controllers/dialogs/RDVDialogController.java
+++ src/gui/controllers/dialogs/RDVDialogController.java
@@ -6,8 +6,10 @@ import gui.windows.RDVWindow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 
+import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 /**
@@ -16,7 +18,15 @@ import java.util.stream.Collectors;
 public class RDVDialogController extends BaseController {
 
     @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Spinner<Integer> durationSpinner;
+    @FXML
     private ComboBox<String> participantsCombobox;
+    @FXML
+    private ListView<String> participantsListview;
+    @FXML
+    private TextField addresstext;
 
     private ObservableList<String> comboboxData;
 
@@ -26,7 +36,7 @@ public class RDVDialogController extends BaseController {
     }
 
     /**
-     * Initializes the combobox of participants.
+     * Initializes the combo box of participants.
      */
     private void initializeCombobox() {
 
@@ -38,6 +48,16 @@ public class RDVDialogController extends BaseController {
                         .collect(Collectors.toList()));
 
         participantsCombobox.setItems(comboboxData);
+        // In case the combo box was empty
+        participantsCombobox.setPlaceholder(new Text("No participants to show."));
+
+        participantsCombobox.getSelectionModel()
+                .selectedItemProperty()
+                .addListener((observable, oldValue, newValue) -> {
+                    if (newValue != null &&
+                            !participantsListview.getItems().contains(newValue))
+                        participantsListview.getItems().add(newValue);
+                });
     }
 
     @FXML
@@ -47,6 +67,14 @@ public class RDVDialogController extends BaseController {
 
     @FXML
     private void onConfirm() {
-        mStage.close();
+        LocalDate date = datePicker.getValue();
+//        int duration = durationSpinner.getValue();
+        String participant = participantsCombobox.getSelectionModel().getSelectedItem();
+        String address = addresstext.getText();
+
+        System.out.println("Date: " + date);
+//        System.out.println("Duration: " + duration);
+        System.out.println("Participant: " + participant);
+        System.out.println("Address: " + address);
     }
 }
